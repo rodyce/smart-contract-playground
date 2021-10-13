c7bbd549b46b898c547277a8618c11feb45a34f7
Rodimiro Cerrato E <rodimiroc@microsoft.com>
2021-10-13 15:12:42 -0600

    Add vaccine passport smart contract

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,10 +1,12 @@
 const ConvertLib = artifacts.require("ConvertLib");
 const MetaCoin = artifacts.require("MetaCoin");
 const SimpleCounter = artifacts.require("SimpleCounter");
+const VaccinePassport = artifacts.require("VaccinePassport");
 
 module.exports = function (deployer) {
   deployer.deploy(ConvertLib);
   deployer.link(ConvertLib, MetaCoin);
   deployer.deploy(MetaCoin);
   deployer.deploy(SimpleCounter);
+  deployer.deploy(VaccinePassport);
 };
